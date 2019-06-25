afe1d5fe6e82a865709dd2a5f52979631eca90f9
Ian <ianpbh.hernandez@gmail.com>
2019-06-25 03:19:57 -0400

    add percentage of clicks reached in click function, ah, also added colors to make the script look cooler

--- cookie.py
+++ cookie.py
@@ -2,45 +2,66 @@ from selenium import webdriver
 import time
 import os
 
-navegador = webdriver.Firefox()
-navegador.get("https://orteil.dashnet.org/cookieclicker/")
-print("Checking your save file and loading the page, don't use the game page until the menu shows up")
-time.sleep(6)
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+browser = webdriver.Firefox()
+browser.get("https://orteil.dashnet.org/cookieclicker/")
+print(colors.WARNING + "Checking your save file and loading the page, don't use the game page until the menu show up" + colors.ENDC)
+time.sleep(4)
+
 if os.path.exists("save.txt"):
-    navegador.find_element_by_id('prefsButton').click()
-    navegador.execute_script("Game.ImportSave()")
+    browser.find_element_by_id('prefsButton').click()
+    browser.execute_script("Game.ImportSave()")
     save = open("save.txt", "r")
-    navegador.find_element_by_id('textareaPrompt').send_keys(save.read())
-    navegador.find_element_by_id('promptOption0').click()
+    browser.find_element_by_id('textareaPrompt').send_keys(save.read())
+    browser.find_element_by_id('promptOption0').click()
+    browser.find_element_by_id('prefsButton').click()
+    print(colors.OKBLUE + "Game loaded!" + colors.ENDC)
 else:
-    print("Save não encontrado")
+    print(colors.OKBLUE + "Save file not found, starting a new game" + colors.ENDC)
 
 def clicaCookie(qnt):
-    quantidade = int(qnt)
-    x = 0
-    while x < quantidade:
-        navegador.execute_script("document.getElementById('bigCookie').click()")
+    quantity = int(qnt)
+    twentyFive = (quantity * 25) / 100
+    fifty = (quantity * 50) / 100
+    seventyFive = (quantity * 75) / 100
+    x = 1
+    while x <= quantity:
+        browser.execute_script("document.getElementById('bigCookie').click()")
+        if x == round(twentyFive):
+            print(colors.OKGREEN + '25%('+ str(round(twentyFive)) +') of ' + qnt + ' clicks reached' + colors.ENDC)
+        elif x == round(fifty):
+            print(colors.OKGREEN + '50%('+ str(round(fifty)) +') of ' + qnt + ' clicks reached' + colors.ENDC)
+        elif x == round(seventyFive):
+            print(colors.OKGREEN + '75%('+ str(round(seventyFive)) +') of ' + qnt + ' clicks reached' + colors.ENDC)
+        elif x == quantity:
+            print(colors.OKGREEN + '100%('+ str(quantity) +') of ' + qnt + ' clicks reached' + colors.ENDC)
         x += 1
 
-def sair():
-    navegador.execute_script("Game.toSave=true")
-    navegador.find_element_by_id('prefsButton').click()
-    navegador.execute_script("Game.ExportSave()")
-    export = navegador.find_element_by_id('textareaPrompt')
+def exitGame():
+    browser.execute_script("Game.toSave=true")
+    browser.find_element_by_id('prefsButton').click()
+    browser.execute_script("Game.ExportSave()")
+    export = browser.find_element_by_id('textareaPrompt')
     arq = open("save.txt", "w+")
     arq.write(export.text)
-    navegador.close()
+    browser.close()
 
 menu = True
 while menu:
-    qnt = input("Type the number of clicks, or 'exit' to save and quit the game: ")
+    qnt = input(colors.OKBLUE + "Type the number of clicks, or 'exit' to save and quit the game: " + colors.ENDC)
     if qnt.upper() == "EXIT":
-            sair()
+            exitGame()
             menu = False
     elif qnt.isdigit():
             clicaCookie(qnt)
     else:
-        print("Invalid option")
-
-
-#<a class="option" onclick="Game.ExportSave();PlaySound('snd/tick.mp3');">Export save</a>
+        print(colors.FAIL + "Invalid option" + colors.ENDC)
